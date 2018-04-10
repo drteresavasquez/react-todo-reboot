29bbb423b74556bc52f99ed9d64055489077e394
Brenda Long <brenda@studio6thave.com>
2018-04-10 05:50:44 -0500

    added link to readme and note in base

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Simple TODO with React and Firebase
+
+Based on re-base from: https://github.com/tylermcginnis/re-base
--- src/base.js
+++ src/base.js
@@ -2,12 +2,18 @@ import Rebase from 're-base'
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyA60-RA1ZHZRXrmmAH-gsp1h7OpRFYWugY",
-    authDomain: "d18-demo.firebaseapp.com",
-    databaseURL: "https://d18-demo.firebaseio.com"
+    apiKey: "REALLYLONGNUMBER",
+    authDomain: "your-database.firebaseapp.com",
+    databaseURL: "https://d18-demo.your-database.com"
 }
 
 const app = firebase.initializeApp(config)
 const base = Rebase.createClass(app.database())
+
+/* for my demo, be sure to put yours in place
+apiKey: "AIzaSyA60-RA1ZHZRXrmmAH-gsp1h7OpRFYWugY",
+authDomain: "d18-demo.firebaseapp.com",
+databaseURL: "https://d18-demo.firebaseio.com"
+*/
 
 export default base;
